7f2eb748b7e15d46f197ca465ae736d4b5afd72d
Rifat Rabbi <rifayrabby78@gmail.com>
2023-10-07 01:26:03 +0600

    Details page design and show completed

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -4,10 +4,22 @@ const Details = () => {
   const { id } = useParams();
   const datas = useLoaderData();
   const dublicate = datas.find((data) => data.id === id);
-  const { name } = dublicate;
+  const { name, price, details, image } = dublicate;
   return (
     <div>
-      <h1>This is Details{name}</h1>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row mx-20 gap-5">
+          <div>
+            <img src={image} className="max-w-md rounded-lg shadow-2xl" />
+          </div>
+          <div className="w-full ">
+            <h1 className="text-5xl font-bold">{name}</h1>
+            <p className="py-6">{details}</p>
+            <p className="pb-6 font-bold">Price: {price} $</p>
+            <button className="btn btn-primary">Join Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
